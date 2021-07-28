9533d58ced106ea3889f735d6e5971e92685fcc5
eunseoJeong <em4784@gmail.com>
2021-07-28 15:26:18 +0900

    개발 :: result앱 수정 (정은서)

--- result/urls.py
+++ result/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 from result import views as R
 
 urlpatterns = [
-    path('<str:id>',R.detail, name="detail"),
-    path('post/', R.make_post, name="post"),
-    path('update/<str:id>', R.update, name="update"),
-    path('delete/<str:id>', R.delete, name="delete"),
+    path('<str:id>/',R.detail, name="detail"),
+    path('post/', R.make_post, name="make_post"),
+    path('update/<str:id>/', R.update, name="update"),
+    path('delete/<str:id>/', R.delete, name="delete"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
