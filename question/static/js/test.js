f457da370b414bf1ab0679c9db75df57a99998de
kiteB <mmy789@sch.ac.kr>
2021-07-30 02:53:19 +0900

    수정 :: 마지막 결과 확인 페이지 CSS 수정 (배연주)

--- question/static/js/test.js
+++ question/static/js/test.js
@@ -1,14 +1,14 @@
 // #btn 버튼을 클릭했을 때
-$("#btn").click(function () {
+$("#answer-btn1").click(function () {
     // #result_page의 display가 "none"일 경우 (접혀있는 경우)
     if ($("#result_page").css("display") == "none") {
-        // #more 버튼 감추기
-        $("#btn").hide();
-        $("#btn2").hide();
+        // 감추기
+        $(".num").hide();
+        $(".que").hide();
+        $(".box").hide();
+        $(".box_ddu").hide();
+        $("#page_info").hide();
         // #result_page 보이기
         $("#result_page").show();
-    } else {
-        // #result_page의 display가 "block"인 경우 (펼쳐져 있는 경우)
-        $("#btn").show();
     }
 });
