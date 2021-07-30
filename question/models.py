f5eaf60fb9c6c0ee0701ad5f59a340966eddc2ee
eunseoJeong <em4784@gmail.com>
2021-07-30 15:41:40 +0900

    최종 :: 답변 TextField로 수정(모델), 줄바꿈 추가, 사진 사이즈조정 (정은서)

--- question/models.py
+++ question/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Question(models.Model):
     que = models.CharField(max_length=200)
-    answer1 = models.CharField(max_length=50)
-    answer2 = models.CharField(max_length=50)
+    answer1 = models.TextField()
+    answer2 = models.TextField()
     page = models.IntegerField()
